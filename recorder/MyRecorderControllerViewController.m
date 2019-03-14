e87e485a2513d25e2ea0918528fb8b86e649358d
wangweiwei <wangjiecdma@126.com841004>
2019-03-14 15:46:28 +0800

    first commit

---
+++ recorder/MyRecorderControllerViewController.m
@@ -0,0 +1,32 @@
+//
+//  MyRecorderControllerViewController.m
+//  recorder
+//
+//  Created by wangweiwei on 2019/3/11.
+//  Copyright © 2019 wangweiwei. All rights reserved.
+//
+
+#import "MyRecorderControllerViewController.h"
+
+@interface MyRecorderControllerViewController ()
+
+@end
+
+@implementation MyRecorderControllerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
